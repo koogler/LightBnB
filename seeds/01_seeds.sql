052a907555f062ffa3979ff6f523136b5dfcb54c
Desmond Parker <evan.parker95@gmail.com>
2022-01-06 22:56:14 +0000

    updated getUserWithEmail, getUserWithId, and addUser to function with queries to the database

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -8,11 +8,11 @@ VALUES
 
 INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url,cost_per_night,parking_spaces,number_of_bathrooms,number_of_bedrooms,country,street,city,province,post_code)
 VALUES
-(1, 'Castle Vlad', 'description', 'https://imgur.com/t/aww/6ERe7Gb', 'https://imgur.com/t/aww/6ERe7Gb', 900, 5, 2, 4, 'Canada', '123 Fake Street', 'Toronto', 'Ontario', 
+(1, 'Castle Vlad', 'description', 'https://i.imgur.com/XwRGAZo.jpeg', 'https://i.imgur.com/XwRGAZo.jpeg', 900, 5, 2, 4, 'Canada', '123 Fake Street', 'Toronto', 'Ontario', 
 'L2A2P3'),
-(2, 'McDonalds Basement', 'description', 'https://imgur.com/t/aww/6ERe7Gb', 'https://imgur.com/t/aww/6ERe7Gb', 400, 22, 1, 1, 'Canada', '642 Front Avenue', 'Toronto', 'Ontario', 
+(2, 'McDonalds Basement', 'description', 'https://i.imgur.com/XwRGAZo.jpeg', 'https://i.imgur.com/XwRGAZo.jpeg', 400, 22, 1, 1, 'Canada', '642 Front Avenue', 'Toronto', 'Ontario', 
 'F5P6G1'),
-(3, 'A Real House', 'description', 'https://imgur.com/t/aww/6ERe7Gb', 'https://imgur.com/t/aww/6ERe7Gb', 2000, 1, 2, 3, 'Canada', '534 Point Road', 'Toronto', 'Ontario', 
+(3, 'A Real House', 'description', 'https://i.imgur.com/XwRGAZo.jpeg', 'https://i.imgur.com/XwRGAZo.jpeg', 2000, 1, 2, 3, 'Canada', '534 Point Road', 'Toronto', 'Ontario', 
 'H9F4N5');
 
 INSERT INTO reservations (guest_id, property_id, start_date, end_date)
