69907642e2711c85cc6e785d0a3684e01eb88b78
Desmond Parker <evan.parker95@gmail.com>
2022-02-11 23:57:56 +0000

    adding better search functionality to database.js

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -16,6 +16,7 @@ const pool = new Pool({
  * @param {String} email The email of the user.
  * @return {Promise<{}>} A promise to the user.
  */
+
 const getUserWithEmail = function (email) {
   return pool
     .query(`
@@ -29,7 +30,10 @@ const getUserWithEmail = function (email) {
       return null;
     });
 }
+
 exports.getUserWithEmail = getUserWithEmail;
+
+
 
 /**
  * Get a single user from the database given their id.
@@ -107,13 +111,65 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 
-const getAllProperties = (options, limit = 10) => {
-  return pool
-    .query(`SELECT * FROM properties LIMIT $1`, [limit])
-    .then((result) => result.rows)
-    .catch((err) => {
-      console.log(err.message);
-    });
+const getAllProperties = function (options, limit = 10) {
+  // 1
+  const queryParams = [];
+  // 2
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  `;
+
+  // 3
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+
+    queryString += `WHERE city LIKE $${queryParams.length} `;
+
+  }
+
+  if (options.owner_id) {
+    queryParams.push(`${options.owner_id}`);
+
+    queryString += `AND properties.owner_id = $${queryParams.length} `;
+
+  }
+
+  if (options.minimum_price_per_night) {
+    queryParams.push(parseFloat(options.minimum_price_per_night) * 100);
+
+    queryString += `AND properties.cost_per_night >= $${queryParams.length} `;
+
+  }
+
+  if (options.maximum_price_per_night) {
+    queryParams.push(parseFloat(options.maximum_price_per_night) * 100);
+
+    queryString += `AND properties.cost_per_night <= $${queryParams.length} `;
+
+  }
+
+  if (options.minimum_rating) {
+    queryParams.push(options.minimum_rating);
+
+    queryString += `AND rating >= $${queryParams.length} `;
+
+  }
+
+  // 4
+  queryParams.push(limit);
+  queryString += `
+  GROUP BY properties.id
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+
+  // 5
+  console.log(queryString, queryParams);
+
+  // 6
+  return pool.query(queryString, queryParams).then((res) => res.rows);
 };
 
 
--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -19,7 +19,8 @@ INSERT INTO reservations (guest_id, property_id, start_date, end_date)
 VALUES 
 (1, 1, '2018-09-11', '2018-09-26'),
 (2, 2, '2019-01-04', '2019-02-01'),
-(3, 3, '2021-10-01', '2021-10-14');
+(3, 3, '2021-10-01', '2021-10-14'),
+(1006, 3, '2021-10-01', '2021-10-14');
 
 INSERT INTO property_reviews (guest_id, property_id, reservation_id, rating, message)
 VALUES
